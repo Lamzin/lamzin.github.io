5fafbc694ceca520fbe22e75472af5f0bd242fba
Oleh Lamzin <olehtomas@gmail.com>
2020-05-17 22:03:12 +0200

    Create very basic note app

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
 /* Only register a service worker if it's supported */
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/service-worker.js");
+  navigator.serviceWorker.addEventListener("message", onTextAreaLoad);
 }
 
-window.addEventListener("beforeinstallprompt", event => {
-  console.log("beforeinstallprompt", event);
+window.addEventListener("DOMContentLoaded", e => {
+  document.getElementById("mainTextArea").addEventListener("input", event => {
+    onTextAreaChange();
+  });
+  requestTextAreaNote();
 });
+
+function requestTextAreaNote() {
+  if (!("serviceWorker" in navigator)) {
+    return;
+  }
+  if (!navigator.serviceWorker.controller) {
+    return;
+  }
+
+  navigator.serviceWorker.controller.postMessage({
+    command: "load"
+  });
+}
+
+function onTextAreaLoad(event) {
+  document.getElementById("mainTextArea").value = event.data.notes;
+}
+
+function onTextAreaChange() {
+  let notes = document.getElementById("mainTextArea").value;
+
+  if (!("serviceWorker" in navigator)) {
+    console.log("Service worker is not supported");
+    return;
+  }
+  if (!navigator.serviceWorker.controller) {
+    console.log("This page is not currently controlled by a service worker.");
+    return;
+  }
+
+  navigator.serviceWorker.controller.postMessage({
+    command: "save",
+    notes: notes
+  });
+}
--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,59 @@
 self.addEventListener("install", event => {
-  console.log("install", event);
   self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  console.log("activate", event);
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", function(event) {
-  console.log("fetch", event);
+self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
+
+self.addEventListener("message", (event, callback) => {
+  if (event.data.command == "save") {
+    putNotes(event.data.notes);
+  }
+  if (event.data.command == "load") {
+    getNotes();
+  }
+});
+
+function putNotes(notes) {
+  let open = indexedDB.open("notes", 1);
+
+  open.onupgradeneeded = function() {
+    let db = open.result;
+    db.createObjectStore("note", { autoIncrement: true });
+  };
+
+  open.onsuccess = function() {
+    let db = open.result;
+    let store = db.transaction("note", "readwrite").objectStore("note");
+    store.put(notes, 1);
+  };
+}
+
+function getNotes() {
+  let open = indexedDB.open("notes", 1);
+
+  open.onupgradeneeded = function() {
+    let db = open.result;
+    db.createObjectStore("note", { autoIncrement: true });
+  };
+
+  open.onsuccess = function() {
+    let db = open.result;
+    let store = db.transaction("note", "readwrite").objectStore("note");
+    request = store.get(1);
+
+    request.onsuccess = function(event) {
+      let notes = event.target.result;
+      self.clients.matchAll().then(clients => {
+        clients.forEach(client => {
+          client.postMessage({ notes: notes });
+        });
+      });
+    };
+  };
+}
